b9e41cb0041019d28b77eda761c7ff2b95e4ef4e
jim112828 <jim112828@hotmail.com>
2017-06-03 22:48:48 +0800

    btw commit
    btw commit

--- MiraclePills/ViewController.swift
+++ MiraclePills/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate{
 
+  
+    @IBOutlet weak var buyNowBtn: UIButton!
     @IBOutlet weak var statePickerBtn: UIButton!
 
     @IBOutlet weak var statePicker: UIPickerView!
@@ -28,7 +30,8 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
     }
 
     @IBAction func stateBtnPressed(_ sender: Any) {
-    }
+        statePicker.isHidden = false
+        }
      
  
         func numberOfComponents(in pickerView: UIPickerView) -> Int {
@@ -42,8 +45,14 @@ class ViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelega
         func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
             return states[row]
         }
-        
     
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState.normal)
+        statePicker.isHidden = true
+    }
+    
+    @IBAction func buyNow(_ sender: Any) {
+    }
 
     
         
